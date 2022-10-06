b39486edcf62ae2596a74f33a760066ff6677c4a
LDAP\ttdat4 <ttdat4@cmcglobal.vn>
2022-10-06 17:03:44 +0700

    update code

---
+++ src/main/java/com/learn/restfulwebservices/post/Post.java
@@ -0,0 +1,38 @@
+package com.learn.restfulwebservices;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.learn.restfulwebservices.user.User;
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    public Post() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/learn/restfulwebservices/post/PostRepository.java
@@ -0,0 +1,2 @@
+package com.learn.restfulwebservices.post;public class PostRepository {
+}
